04e67b119abcf973b3319f7640c3ef4b7ed89c77
yule93 <dbfm2222@gmail.com>
2021-02-06 15:37:07 +0900

    원상복구 드디어 다 함....

---
+++ src/Components/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  border-bottom: 3px solid ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
     <>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
     </>
   </Router>
 );
