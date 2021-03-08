49e5e55362248451fbe7507216f4652aa3cdf516
yule93 <dbfm2222@gmail.com>
2021-03-08 22:00:19 +0900

    add Section that shows a lot of movies

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,8 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
 
-const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) => null;
+const Container = styled.div``;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+loading ? null : (
+    <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+            <Section title = "Now Playing" >
+                {nowPlaying.map(movie => movie.title)}
+            </Section>
+        )}
+        {popular && popular.length > 0 && (
+            <Section title = "Popular Movies" >
+                {popular.map(movie => movie.title)}
+            </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+            <Section title = "Upcoming" >
+                {upcoming.map(movie => movie.title)}
+            </Section>
+        )}
+    </Container>
+);
 
 HomePresenter.PropTypes = {
     nowPlaying: PropTypes.array,
